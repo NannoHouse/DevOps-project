f1b8630dc2e1c52835256e65eac65aadffe8ae9f
NannoHouse <gergana.yordanovaa03@gmail.com>
2025-01-05 12:23:37 +0200

    reordering code to follow flake8 standarts

--- src/app.py
+++ src/app.py
@@ -3,11 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def random_number():
     rand_num = random.randint(1, 1000)
 
     return f"Random Number: {rand_num}"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
